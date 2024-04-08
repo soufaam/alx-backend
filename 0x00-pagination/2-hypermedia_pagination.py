f0be93a3ca51352f723c7345b6bcfcf5a452d740
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-08 13:09:23 +0000

    fix pyocoe style

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -63,7 +63,7 @@ class Server:
             dictionnay['next_page'] = None
         return dictionnay
 
-    def get_hyper_index(index = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(index=None, page_size: int = 10) -> Dict:
         """get_hyper_index method with two integer arguments:
         index with a None default value and page_size 
         with default value of 10."""
