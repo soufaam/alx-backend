3faadaa8088f393cff53b22ba673be867c35811d
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-13 23:49:38 +0000

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, url_for, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Class languages"""
+    LANGUAGES = ['en', 'fr']
+
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector)
+app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route("/")
+def basic():
+    """basic Flask app"""
+    return render_template('1-index.html')
