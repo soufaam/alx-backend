b97bb6b71aef4d1012359dc8dea6b21097eca467
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-09 00:21:22 +0100

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,6 +2,7 @@ import kue from 'kue';
 
 const queue = kue.createQueue();
 
+const newqueue = kue.createQueue();
 const obj = {
   phoneNumber: '1234567890',
   message: 'This is a notification message.'
@@ -21,3 +22,14 @@ job.on('failed', function() {
 });
 
 job.save();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+newqueue.process('push_notification_code', (job, done) => {
+    const {phoneNumber, message} = job.data;
+    sendNotification(phoneNumber, message)
+    done();
+});
+
